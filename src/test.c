9d7cfb042dbe407cf73f9860600a10ef0d2e09ae
Enzo M. Congiu <emcongiu@gmail.com>
2015-10-11 23:42:52 +0200

    added splint make target

--- src/test.c
+++ src/test.c
@@ -11,7 +11,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_LENGTH 80
+
 int main(void) {
-	puts("Hello World"); /* prints Hello World */
+	char *buf;
+	int i, n='A';
+
+	buf=malloc(MAX_LENGTH);
+	if(!buf)
+		return EXIT_FAILURE;
+
+	for(i=0;i<MAX_LENGTH; i++) {
+		buf[i]=(char)n;
+		(n=='Z')?n='A':n++;
+	}
+	buf[MAX_LENGTH-1] = '\0';
+
+	(void)puts(buf); /* prints Hello World */
+
+	free(buf);
 	return EXIT_SUCCESS;
 }
